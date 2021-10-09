694dc609482878334d969940abe3f60739fe5d95
moneer824 <moneerashraf824@gmail.com>
2021-10-09 14:31:15 +0530

    ecart practise

---
+++ ecart.js
@@ -0,0 +1,126 @@
+let data = [
+    {
+        name: "Red Shoes",
+        price: "2000",
+        img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name: "White Shoes",
+        price: "2500",
+        img: "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHNob2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60"
+    },
+    {
+        name: "Black Shoes",
+        price: "700",
+        img: "https://images.unsplash.com/photo-1494496195158-c3becb4f2475?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+    },
+    {
+        name: "Green Shoes",
+        price: "4000",
+        img: "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hvZXN8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+    },
+    {
+        name: "Yellow Shoes",
+        price: "8000",
+        img: "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    },
+    {
+        name: "Stylish Shoes",
+        price: "3100",
+        img: "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    },
+    {
+        name: "Nike Shoes",
+        price: "5000",
+        img: "https://images.unsplash.com/photo-1605733160314-4fc7dac4bb16?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    },
+    {
+        name: "Casual Shoes",
+        price: "1500",
+        img: "https://images.unsplash.com/photo-1595461135849-bf08893fdc2c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    },
+    {
+        name: "Red Shoes",
+        price: "2000",
+        img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    },
+    {
+        name: "White Shoes",
+        price: "2500",
+        img: "https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHNob2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=60"
+    },
+    {
+        name: "Black Shoes",
+        price: "700",
+        img: "https://images.unsplash.com/photo-1494496195158-c3becb4f2475?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+    },
+    {
+        name: "Green Shoes",
+        price: "4000",
+        img: "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hvZXN8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"
+    },
+    {
+        name: "Yellow Shoes",
+        price: "8000",
+        img: "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    },
+    {
+        name: "Stylish Shoes",
+        price: "3100",
+        img: "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    },
+    {
+        name: "Nike Shoes",
+        price: "5000",
+        img: "https://images.unsplash.com/photo-1605733160314-4fc7dac4bb16?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    },
+    {
+        name: "Casual Shoes",
+        price: "1500",
+        img: "https://images.unsplash.com/photo-1595461135849-bf08893fdc2c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHNob2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=60"
+    }
+]
+
+let parent = document.getElementById("product")
+
+function showfunction() {
+
+    data.forEach(function (product) {
+        // console.log(product.name);
+
+        let div = document.createElement("div")
+        let img = document.createElement("img")
+        img.src = product.img
+        let product_price = document.createElement("p")
+        product_price.textContent = "Price ₹" + product.price
+        let product_name = document.createElement("p")
+        product_name.textContent = product.name
+        let btn = document.createElement('button')
+        btn.textContent = 'Add To Cart'
+        btn.addEventListener('click', addto)
+        function addto() {
+            addtocart(product)
+        }
+        // btn.addEventListener('click', addto)
+
+
+
+
+
+        div.append(img, product_name, product_price, btn)
+        parent.append(div)
+    })
+}
+showfunction()
+
+if (localStorage.getItem('cart') === null) {
+    localStorage.setItem("cart", JSON.stringify([]))
+}
+
+function addtocart(product) {
+    product_cart = JSON.parse(localStorage.getItem("cart"))
+    product_cart.push(product)
+    localStorage.setItem("cart", JSON.stringify(product_cart));
+
+    console.log(product_cart)
+}
